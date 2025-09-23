12412e532d3f03fdbef2e6f94b7f8452814c7f13
Abhishek Yadav <abhi1987666@gmail.com>
2025-09-23 18:15:28 +0530

    Remove db.js from tracking and add to .gitignore for security

--- Backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const url = "mongodb+srv://madhu:madhu069@cluster0.eu4wlhv.mongodb.net/project"
-
-const connectDb = async () => {
-    await mongoose.connect(url)
-}
-
-module.exports = connectDb
